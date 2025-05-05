6c3dcc1edfc454ae89487a8cacc8fa7993dd4a21
dalloris <dalloris.uz@gmail.com>
2025-05-06 00:22:15 +0500

    ready to change the column names

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import json
 
 # URL to the raw JSON file on GitHub
 url = "https://github.com/odilbekmarimov/DemoProject/raw/main/column_table_map.json"
+
 # Load the JSON file into a DataFrame using pandas
 df = pd.read_json(url)
 
-# Display the DataFrame
-print("DataFrame:")
-print(df)
-
-# Save the DataFrame to a new JSON file
-output_file = "output.json"
+output_file = "column_table_map.json"
 df.to_json(output_file, orient="records")
+
+print(f"\nDataFrame saved in pretty format as '{output_file}'")
 
 # Define a dictionary with file names and corresponding URLs from GitHub
 files_urls = {
-    "t01.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t01.csv",
-    "t02.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t02.csv",
-    "t03.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t03.csv",
-    "t04.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t04.csv",
-    "t05.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t05.csv",
-    "t07.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files/t07.csv"
+    "t01.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t01.csv",
+    "t02.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t02.csv",
+    "t03.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t03.csv",
+    "t04.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t04.csv",
+    "t05.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t05.csv",
+    "t07.csv": "https://raw.githubusercontent.com/odilbekmarimov/DemoProject/main/files_final/t07.csv"
 }
 
 # Download and save the CSV files excluding t06.csv
